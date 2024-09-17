0f114cd832d61cffcab5f2f34cf77c64855f6dc9
akiracopeland <akiracopeland713@gmail.com>
2024-09-17 16:28:50 -0700

    CloudWatch Metric Observation Aggregator quiz done

--- src/com/amazon/ata/comparable_comparator_sort/UserPageVisit.java
+++ src/com/amazon/ata/comparable_comparator_sort/UserPageVisit.java
@@ -35,16 +35,12 @@ public class UserPageVisit implements Comparable<UserPageVisit> {
         return timeOnPageInSeconds;
     }
 
-    /**
-     * The method that checks if this object is equal to another object. If the object is of same type,
-     * the fields that are compared for equality are userId and page. Field timeOnPageInSeconds is ignored.
-     * @param o - object to compare this object against.
-     * @return true if objects are equal, false otherwise.
-     */
     @Override
     public boolean equals(Object o) {
-        // PARTICIPANTS: implement equals method here (hint: you can use intellij's auto-generate).
-        return true;
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserPageVisit that = (UserPageVisit) o;
+        return userId == that.userId && Objects.equals(page, that.page);
     }
 
     @Override
@@ -66,7 +62,15 @@ public class UserPageVisit implements Comparable<UserPageVisit> {
     @Override
     public int compareTo(UserPageVisit other) {
         // PARTICIPANTS: implement compareTo method here
-        return 0;
+
+        if (this.userId < other.userId) {
+            return -1;
+        }
+        else if (this.userId > other.userId) {
+            return 1;
+        }else {
+            return this.page.compareTo(other.page);
+            }
     }
 
     @Override
--- src/com/amazon/ata/comparable_comparator_sort/UserPageVisitGrouper.java
+++ src/com/amazon/ata/comparable_comparator_sort/UserPageVisitGrouper.java
@@ -1,6 +1,7 @@
 package com.amazon.ata.comparable_comparator_sort;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,7 +16,9 @@ public class UserPageVisitGrouper {
      */
     public List<UserPageVisit> groupUserPageVisits(List<UserPageVisit> userPageVisits) {
         // PARTICIPANTS: add logic to implement groupUserPageVisits here
-        List<UserPageVisit> userPageVisitsCopy = new ArrayList<>();
+        List<UserPageVisit> userPageVisitsCopy = new ArrayList<>(userPageVisits);
+
+        Collections.sort(userPageVisitsCopy);
 
         return userPageVisitsCopy;
     }
